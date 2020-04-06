ffc4b58e49f827fc282ed8618812c3364a4782a0
bernard-ng <ngandubernard@gmail.com>
2020-04-06 04:38:12 +0200

    feat: Add JHU API Service

--- src/Services/JHUAPIService.php
+++ src/Services/JHUAPIService.php
@@ -3,6 +3,10 @@
 namespace App\Services;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
@@ -13,6 +17,17 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class JHUAPIService
 {
     public static string $url = 'https://covid19.mathdro.id/api';
+
+    public static array $endpoint = [
+        'global' => '',
+        'og' => '/og',
+        'confirmed' => '/confirmed',
+        'recovered' => '/recovered',
+        'deaths' => '/deaths',
+        'daily' => '/daily',
+        'countries' => '/countries'
+    ];
+
     private HttpClientInterface $client;
     private LoggerInterface $logger;
 
@@ -21,20 +36,53 @@ class JHUAPIService
      * @param HttpClientInterface $client
      * @param LoggerInterface $logger
      */
-    public function __construct(
-        HttpClientInterface $client,
-        LoggerInterface $logger
-    ) {
+    public function __construct(HttpClientInterface $client, LoggerInterface $logger)
+    {
         $this->client = $client;
         $this->logger = $logger;
     }
 
     public function getGlobalSummary()
     {
-        try {
-            $response = $this->client->request('GET', self::$url);
+        return $this->processRequest(function () {
+            $response = $this->client->request('GET', $this->getUrl('global'));
             return $response->getContent();
-        } catch (\Exception $e) {
+        });
+    }
+
+    public function getOGImage()
+    {
+        return $this->processRequest(function () {
+            $response = $this->client->request('GET', $this->getUrl('og'));
+            return $response->getContent();
+        });
+    }
+
+    /**
+     * Generate resource url
+     * @param string $string
+     * @return string
+     * @author bernard-ng <ngandubernard@gmail.com>
+     */
+    private function getUrl(string $string): string
+    {
+        return self::$url . self::$endpoint[$string];
+    }
+
+    /**
+     * @param callable $action
+     * @return mixed
+     * @author bernard-ng <ngandubernard@gmail.com>
+     */
+    private function processRequest(callable $action)
+    {
+        try {
+            return $action();
+        } catch (TransportExceptionInterface |
+        ClientExceptionInterface |
+        RedirectionExceptionInterface |
+        ServerExceptionInterface $e
+        ) {
             $this->logger->error($e->getMessage(), $e->getTrace());
         }
     }
